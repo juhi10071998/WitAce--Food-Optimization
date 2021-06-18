f151a006e6bbf802b82574fda7f63abcb047982d
Ishika11 <41107336+Ishika11@users.noreply.github.com>
2021-06-18 10:35:36 +0530

    Add files via upload

---
+++ eCommerceFlask/app_ecommerce/categories/routes.py
@@ -0,0 +1,43 @@
+from flask import (render_template, url_for, flash,
+                   redirect, Blueprint)
+from flask_login import login_required
+from app_ecommerce.categories.utils import get_categories
+from app_ecommerce.categories.forms import CategoryForm
+from app_ecommerce.products.utils import get_quote_USD_to
+from app_ecommerce.models import Category
+from app_ecommerce import db,quote
+
+categories = Blueprint('categories',__name__)
+
+@categories.route('/category/new',methods=['GET','POST'])
+@login_required
+def new_category():
+    form = CategoryForm()
+    form.parent_category.choices = get_categories()
+    if form.validate_on_submit():
+        img = None
+        
+        par_cat = None
+        if form.parent_category.data:
+            par_cat = Category.query.get(form.parent_category.data)
+        c = Category(name=form.name.data,
+                     
+                     parent_cat=par_cat)
+        db.session.add(c)
+        db.session.commit()
+        flash(f'Your category has been created', 'success')
+        return redirect(url_for('main.home'))
+
+    return render_template('create_category.html', title='New Category', form=form, legend='New Category')
+
+@categories.route('/category/<int:category_id>')
+def subcategories_for_category(category_id):
+    categories = Category.query.get(category_id).subCategories
+    return render_template('home.html', categories=categories)
+
+@categories.route('/category/<int:category_id>/products')
+def products_for_category(category_id):
+    c = Category.query.get(category_id)
+    products = c.products
+    categories = [c]
+    return render_template('home.html',categories=categories, products=products,quote=quote)
---
+++ eCommerceFlask/app_ecommerce/categories/utils.py
@@ -0,0 +1,18 @@
+from app_ecommerce.models import Category
+
+def get_categories_allowed():
+    categories = Category.query.all()
+    output = []
+    for cat in categories:
+        if len(cat.subCategories) == 0:
+            output.append((cat.id,cat.name))
+    return output
+
+def get_categories():
+    categories = Category.query.all()
+    output = [(None,'Superior category')]
+    for cat in categories:
+        output.append((cat.id,cat.name))
+    return output
+
+
